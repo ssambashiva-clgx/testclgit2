ef3e56f0f0010aa8a6050dee52a9447bbb3fd4b3
ssambashiva-clgx <ssambashiva@corelogic.com>
2022-06-23 14:52:43 +0530

    first commit

---
+++ src/main/java/com/core/TestclgitApplication.java
@@ -0,0 +1,17 @@
+package com.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestclgitApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("TestclgitApplication->main()");
+
+		SpringApplication.run(TestclgitApplication.class, args);
+
+	}
+
+}
